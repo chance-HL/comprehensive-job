a1dc01e9c68cd8a973efdeeba75b9b8ab5877413
chance <1175534176@qq.com>
2022-03-04 23:39:20 +0800

    chore(删除多余svg)

--- src/components/lb-table/lb-render.vue
+++ src/components/lb-table/lb-render.vue
@@ -12,8 +12,14 @@ export default {
   name: 'LbRender',
   functional: true,
   props: {
-    scope: Object,
-    render: Function
+    scope: {
+      type: Object,
+      required: true
+    },
+    render: {
+      type: Function,
+      required: true
+    }
   },
   render: (h, ctx) => {
     return ctx.props.render ? ctx.props.render(h, ctx.props.scope) : ''
